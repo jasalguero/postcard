9eeb5eaf3a2bf86a87f15c57423e38004200d348
jasalguero <jasalguero@gmail.com>
2015-04-28 01:48:28 +0200

    First commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var messageInput: UITextField!
+    @IBOutlet weak var sendMailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleSendMail(sender: UIButton) {
+        sendMailButton.setTitle("Mail Sent!", forState: UIControlState.Normal);
+    }
+    
+    func checkButtonState() {
+        if (nameLabel.text?.isEmpty == true || messageLabel.text?.isEmpty == true) {
+            sendMailButton.enabled = false;
+        } else {
+            sendMailButton.enabled = true;
+        }
+    }
 
+    @IBAction func handleNameChanged(sender: UITextField) {
+        nameLabel.hidden = sender.text.isEmpty;
+        nameLabel.text = sender.text;
+        
+        checkButtonState();
+        
+    }
+    
+    @IBAction func handleMessageChanged(sender: UITextField) {
+        messageLabel.hidden = sender.text.isEmpty;
+        messageLabel.text = sender.text;
+        
+        checkButtonState();
+    }
+    
 }
 
